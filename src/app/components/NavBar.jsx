8edfbc77d1b9c8276ab7d51e426b6aebd5336470
Jakob Bal <baljakob9@gmail.com>
2024-09-10 14:23:19 +0200

    fix: redid merge of credentials into main

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,24 +1,22 @@
-"use client";
-import {CgProfile} from "react-icons/cg";
-import {IoMdSettings} from "react-icons/io";
-import {TbLogin2, TbLogout} from "react-icons/tb";
-import {PiVaultBold} from "react-icons/pi";
-import LoginForm from "@/app/components/LoginForm";
-import {useState} from "react";
+'use client';
+import { CgProfile } from 'react-icons/cg';
+import { IoMdSettings } from 'react-icons/io';
+import { TbLogin2, TbLogout } from 'react-icons/tb';
+import { PiVaultBold } from 'react-icons/pi';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { useEffect, useState } from 'react';
+import { logout } from '../services/user-service';
 
 export default function NavBar() {
-    const [isDropdownVisible, setIsDropdownVisible] = useState(false);
-    const [isLoginOpen, setIsLoginOpen] = useState(false);
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const [user, setUser] = useState(null);
 
-    const toggleDropdown = () => {
-        setIsDropdownVisible(!isDropdownVisible);
-    };
-    const handleOpenLogin = () => {
-        setIsLoginOpen(true);
-    };
-    const handleCloseModal = () => {
-        setIsLoginOpen(false);
-    };
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+    if (user) {
+      setUser(JSON.parse(user));
+    }
+  }, []);
 
   const toggleDropdown = () => {
     setIsDropdownVisible(!isDropdownVisible);
@@ -68,51 +66,45 @@ export default function NavBar() {
                       <IoMdSettings size={24} className={'inline-block'} />{' '}
                       Settings
                     </a>
-                </p>
-                <button onClick={toggleDropdown}
-                        className={"pl-2 pr-2 pt-2 pb-2 m-2 rounded-full hover:bg-gray-500"}>
-                    <CgProfile size={24}/>
-
-                    {isDropdownVisible &&
-                        (<div className={"absolute mt-3 bg-slate-800"}>
-                                <ul className={"table text-left border-collapse"}>
-                                    <li className={"hover:bg-indigo-300 hover:text-gray-900 p-2 w-32"}>
-                                        <a href="./account"
-                                           className={"table-row"}>
-                                            <IoMdSettings size={24} className={"inline-block"}/> Settings
-                                        </a>
-                                    </li>
-                                    <li className={"hover:bg-indigo-300 hover:text-gray-900 p-2 w-32"}>
-                                        <a href="./vault"
-                                           className={"table-row"}>
-                                            <PiVaultBold size={24} className={"inline"}/> Vault
-                                        </a>
-                                    </li>
-                                    <li className={"hover:bg-indigo-300 hover:text-gray-900 p-2 w-32"}>
-                                        <a href="/"
-                                           className={"table-row"}>
-                                            <TbLogout size={24} className={"inline"}/> Logout
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        )}
-                    {isDropdownVisible &&
-                        (<div className={"hidden mt-3 bg-slate-800"}>
-                                <ul className={"table border-collapse"}>
-                                    <li className={"hover:bg-indigo-300 hover:text-gray-900 p-2 w-32"}>
-                                        <a onClick={handleOpenLogin}
-                                           className={"lg:table-row lg:text-left text-center"}>
-                                            <TbLogin2 size={24} className={"inline"}/> Login
-                                        </a>
-                                    </li>
-                                </ul>
-                                {isLoginOpen && <LoginForm onClose={handleCloseModal}/>}
-                            </div>
-                        )}
-                </button>
-            </section>
-        </nav>
-    )
-        ;
+                  </li>
+                  <li
+                    className={
+                      'hover:bg-indigo-300 hover:text-gray-900 p-2 w-32'
+                    }
+                  >
+                    <a href='./vault' className={'table-row'}>
+                      <PiVaultBold size={24} className={'inline'} /> Vault
+                    </a>
+                  </li>
+                  <li
+                    className={
+                      'hover:bg-indigo-300 hover:text-gray-900 p-2 w-32'
+                    }
+                  >
+                    <a href='/' className={'table-row'} onClick={logoutUser}>
+                      <TbLogout size={24} className={'inline'} /> Logout
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            )}
+            <div className={'hidden mt-3 bg-slate-800'}>
+              <ul className={'table border-collapse'}>
+                <li
+                  className={'hover:bg-indigo-300 hover:text-gray-900 p-2 w-32'}
+                >
+                  <a
+                    href='/'
+                    className={'lg:table-row lg:text-left text-center'}
+                  >
+                    <TbLogin2 size={24} className={'inline'} /> Login
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </button>
+        )}
+      </section>
+    </nav>
+  );
 }
